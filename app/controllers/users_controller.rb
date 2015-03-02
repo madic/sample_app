aebf73c0a761dc52b5c0d909ee95aab31d76e25f
Milos <milos.madic@propulsionapp.com>
2015-03-02 12:38:01 +0100

    activation https://www.railstutorial.org/book/account_activation_password_reset#sec-account_activations_resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   attr_accessor :name, :email
 
 def show
@@ -26,6 +27,10 @@ end
     end
   end
 
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -40,7 +45,11 @@ end
     @user = User.find(params[:id])
   end
 
-
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
